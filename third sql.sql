5fdadbef262b24111da0a07aae8b06e1050bc859
Raveen-Narendra <raveen1997@gmail.com>
2017-07-23 15:25:24 +0530

    Create third sql.sql

---
+++ third sql.sql
@@ -0,0 +1,13 @@
+drop database company;
+create database company;
+select database();
+use company;
+slect database();
+create table Employee(
+    name varchar(15) not null,
+    id int not null primary key;
+    age int);
+describe Employee;
+insert into Employee values('Raveen',12,20);
+select * from Employee;
+    
