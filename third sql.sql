3ae03d122daf095f483be55ff9b398e6bfaa2850
Raveen-Narendra <raveen1997@gmail.com>
2017-07-23 15:27:07 +0530

    Update third sql.sql

--- third sql.sql
+++ third sql.sql
@@ -2,10 +2,10 @@ drop database company;
 create database company;
 select database();
 use company;
-slect database();
+select database();
 create table Employee(
     name varchar(15) not null,
-    id int not null primary key;
+    id int not null primary key,
     age int);
 describe Employee;
 insert into Employee values('Raveen',12,20);
